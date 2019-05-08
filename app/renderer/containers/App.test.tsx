f875a6a3c3ca3a0a6f97fc540403e638b4029218
wangxuefeng <xuefeng.wang@ctrip.com>
2019-05-08 14:59:36 +0800

    enzyme test included

--- app/renderer/containers/App.test.tsx
+++ app/renderer/containers/App.test.tsx
@@ -4,10 +4,11 @@ import App from './App';
 import {configureStore, history} from '../store';
 import {ConnectedRouter} from 'connected-react-router';
 import {Provider} from 'react-redux';
+import { shallow } from 'enzyme';
 
+const store = configureStore();
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const store = configureStore();
   ReactDOM.render(<Provider store={store}>
     <ConnectedRouter history={history}>
       <App />
@@ -15,3 +16,21 @@ it('renders without crashing', () => {
   </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Enzyme Shallow', () => {
+  it('App\'s title should be Todos', () => {
+    const app = shallow(<Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>);
+    expect(app.find('div').length).toEqual(0);
+  });
+});
+
+// describe('testing', () => {
+//   it('App\'s title should be Todos', () => {
+//     // const app = shallow(<App/>);
+//     expect(1).toEqual(1);
+//   });
+// }
